52484c9f98b77948c335e9cbe41c2141d884e774
Ludodo <31474197+Ludodo2002@users.noreply.github.com>
2022-08-03 16:07:16 +0200

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -5,17 +5,43 @@ const placeholder = [
     {"id":newId(),"title": "Bon fichier ?", "completed": false},
     {"id":newId(),"title": "Tout sur git ?", "completed": false},
     {"id":newId(),"title": "Git commit/push fait ?", "completed": false},
+        {"id":newId(),"title": "Norminette ?", "completed": false},
+
 ]
 
+function sendMessages(){
+    const request = new XMLHttpRequest();
+      request.open("POST", "https://discord.com/api/webhooks/1003575670254862386/A65STpIOjkUAQPLLC-hWUQhvVOt95THzwvJu7ZgIYfNwlmgSQ4tR7w_Zk4BmriujJN7a");
 
+      request.setRequestHeader('Content-type', 'application/json');
+
+      const params = {
+        username: "Site web",
+        avatar_url: "",
+        content: "Une personne vient de ce connecter au site"
+      }
+
+      request.send(JSON.stringify(params));
+    }
 function setup() {
     if (localStorage.getItem("todo")) load(JSON.parse(localStorage.getItem("todo")));
     else load(placeholder);
     $("#addForm").on("submit", (e)=>{
         e.preventDefault();
-        addTodo($("#newTodoInput").val());
-        $("#newTodoInput").val("");
+        addInput();
     });
+    $("#newTodoBtn").on("click", (e)=>{
+        addInput();
+    });
+    $("#resetTodo").on("click", (e)=>{
+        reset();
+    });
+    sendMessages();
+}
+
+function addInput() {
+    addTodo($("#newTodoInput").val());
+    $("#newTodoInput").val("");
 }
 
 function load(data) {
@@ -28,7 +54,7 @@ function createTodo(todo) {
     todoElement.on("click", ()=>{ checkTodo(todo.id, todoElement); });
     if (todo.completed) todoElement.addClass("checked");
     todoElement.text(todo.title);
-    let deleteBtn = $(`<span class="close">X</span>`);
+    let deleteBtn = $(`<span class="close">\u00D7</span>`);
     deleteBtn.on("click", ()=>{ deleteTodo(todo.id, todoElement); });
     deleteBtn.appendTo(todoElement);
     todos.push(todo);
@@ -36,9 +62,15 @@ function createTodo(todo) {
 }
 
 function addTodo(title) {
-    let todo = { id: newId(), title: title, completed: false };
-    $("ul#list").append(createTodo(todo));
-    save();
+    if(title == ''){
+        alert('Le titre est invalide.');
+        return;
+    }else {
+        let todo = { id: newId(), title: title, completed: false };
+        $("ul#list").append(createTodo(todo));
+        save();
+    }
+    
 }
 
 function deleteTodo(id, element) {
@@ -51,6 +83,12 @@ function checkTodo(id ,element) {
     $(element).toggleClass("checked");
     todos[getTodoById(id)].completed = !todos[getTodoById(id)].completed;
     save();
+}
+
+function reset(){
+   localStorage.clear();
+   location.reload();
+    console.log('test');
 }
 
 function newId() { return Math.floor(Math.random() * 999); }
